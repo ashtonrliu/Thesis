a26741e850b5dbe28aae26fb77e2adc1e004fba6
Ashton Liu <ashtonrliu@gmail.com>
2025-03-28 14:33:40 +1100

    Creates directories if doesn't exist

--- data_handler.py
+++ data_handler.py
@@ -2,13 +2,23 @@ from data_sources.Yahoofinance.yahoo_streaming import Yahoo_Streaming
 from utils.export import export_df, export_dictionary
 from data_cleaning.json_loader import Json_Loader
 
+import os
+
 class Data_Handler():
     def __init__(self):
         # Initialise Datasources
         self.yahoo = Yahoo_Streaming()
 
-    def create_dataset_directories():
-        
+        self.create_dataset_directories()
+
+    def create_dataset_directories(self):
+        directories = [
+            "datasets/historicals/csv",
+            "datasets/historicals/json"
+        ]
+
+        for dir_path in directories:
+            os.makedirs(dir_path, exist_ok=True)
 
     
     def download_ticker_grouped_json(self, ticker="AAPL", filename="APPLE"):
